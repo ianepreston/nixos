3dd1be2ba864a1ae1425e9b977ef328df92b2546
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2025-06-18 19:10:22 -0600

    add hosts
    Mostly copied over, did minimal updates for this laptop

---
+++ hosts/common/optional/smbclient.nix
@@ -0,0 +1,29 @@
+# create a systemd service to automatically mount the ghost mediashare at boot
+{ inputs, pkgs, ... }:
+let
+  sopsFolder = (builtins.toString inputs.nix-secrets) + "/sops";
+in
+{
+  # required to mount cifs using domain name
+  environment.systemPackages = [ pkgs.cifs-utils ];
+
+  # setup the required secrets
+  sops.secrets = {
+    "smb-secrets" = {
+      sopsFile = "${sopsFolder}/shared.yaml";
+      path = "/etc/nixos/smb-secrets";
+    };
+  };
+
+  fileSystems."/mnt/mediashare" = {
+    device = "//laconia/content";
+    fsType = "cifs";
+    options =
+      let
+        # separate options to prevent hanging on network split
+        # 'noauto'= do not mount via fstab. Will be automounted by systemd
+        separate_options = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in
+      [ "${separate_options},credentials=/etc/nixos/smb-secrets" ];
+  };
+}
