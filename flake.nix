726062d34c95abbd2f1ea7b3d01c6d12519172c4
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2025-06-20 07:17:19 -0600

    try and just get a vanilla flake going

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configs";
+
+  # ...
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    {
+      inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+        # The next two are for pinning to stable vs unstable regardless of what the above is set to
+        # This is particularly useful when an upcoming stable release is in beta because you can effectively
+        # keep 'nixpkgs-stable' set to stable for critical packages while setting 'nixpkgs' to the beta branch to
+        # get a jump start on deprecation changes.
+        # See also 'stable-packages' and 'unstable-packages' overlays at 'overlays/default.nix"
+        nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+        nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+      };
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/nixos/luna/configuration.nix
+          ./hosts/nixos/luna/hardware-configuration.nix
+          # ./nixosModules
+        ];
+      };
+
+    };
+
+}
