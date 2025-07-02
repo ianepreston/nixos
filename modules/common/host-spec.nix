7277fe6e743d856fc2c703c58d64b37e662b46d0
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-07-01 18:33:33 -0600

    revert hostspec
    Need this for NixOS

--- modules/common/host-spec.nix
+++ modules/common/host-spec.nix
@@ -1,11 +1,56 @@
+# Specifications For Differentiating Hosts
 {
-  lib,
   config,
+  pkgs,
+  lib,
   ...
 }:
 {
-  options.hostSpec = lib.mkOption {
-    type = lib.types.attrs;
-    description = "Host-specific configuration";
+  options.hostSpec = {
+    # Data variables that don't dictate configuration settings
+    username = lib.mkOption {
+      type = lib.types.str;
+      description = "The username of the host";
+    };
+    hostName = lib.mkOption {
+      type = lib.types.str;
+      description = "The hostname of the host";
+    };
+    email = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      description = "The email of the user";
+    };
+    userFullName = lib.mkOption {
+      type = lib.types.str;
+      description = "The full name of the user";
+    };
+    handle = lib.mkOption {
+      type = lib.types.str;
+      description = "The handle of the user (eg: github user)";
+    };
+    home = lib.mkOption {
+      type = lib.types.str;
+      description = "The home directory of the user";
+      default =
+        let
+          user = config.hostSpec.username;
+        in
+        if pkgs.stdenv.isLinux then "/home/${user}" else "/Users/${user}";
+    };
+
+    # Configuration Settings
+    isMinimal = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Used to indicate a minimal host";
+    };
+    # Sometimes we can't use pkgs.stdenv.isLinux due to infinite recursion
+    isDarwin = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Used to indicate a host that is darwin";
+    };
   };
+
 }
+
