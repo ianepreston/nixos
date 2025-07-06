52dfa95c6728263248b526c1181c56dfb86d0607
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-07-06 16:30:13 -0600

    explicit cursor
    Just picked one for now, old one broke from testing hyprland somehow

--- home/optional/nixos.nix
+++ home/optional/nixos.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 # Can only set this in NixOS. Home-manager standalone doesn't have stylix
 {
   stylix.targets.neovim.enable = false;
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
 }
