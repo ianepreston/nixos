d739e8ce43d8da58ff661f501e42761fa27f75eb
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-07-07 18:54:39 -0600

    feat: tidy gnome
    Refactor enabling a little
    Remove some apps I don't care about

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -1,15 +1,41 @@
+{ pkgs, ... }:
 {
   # TODO: Break out X11 piece, maybe switch to Wayland
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  services.xserver = {
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+    desktopManager.gnome = {
+      enable = true;
+    };
   };
+  environment.gnome.excludePackages = (
+    with pkgs;
+    [
+      atomix
+      baobab
+      epiphany
+      geary
+      gedit
+      gnome-contacts
+      gnome-calendar
+      gnome-maps
+      gnome-music
+      gnome-photos
+      gnome-tour
+      hitori
+      iagno
+      simple-scan
+      tali
+      yelp
+    ]
+  );
 }
