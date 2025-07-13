ae615d8fe555fc6afe42eb83c79b228f86b61333
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-07-12 18:07:37 -0600

    nvidia tweaks
    Read more of the docs, let's see if this is more stable

---
+++ hosts/common/optional/nvidia-gtx1060.nix
@@ -0,0 +1,21 @@
+{ config, ... }:
+{
+  powerManagement.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    # GTX 1060 is too old to use the open source drivers
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    powerManagement = {
+      enable = true; # See if this helps with sleep/wake issues
+      finegrained = false; # Also trying this for sleep/wake. Should toggle this if the issue persists
+    };
+    prime = {
+      offload.enable = false; # Enable PRIME offloading to integrated GPU
+      sync.enable = true; # Always use nvidia GPU - supposed to be better for clamshell
+      intelBusId = "PCI:00:02:0";
+      nvidiaBusId = "PCI:01:00:0";
+    };
+  };
+}
