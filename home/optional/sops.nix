52336828869ed13a36c1f4a812d67ab19b6a8912
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-07-25 10:50:24 -0600

    patch flake

---
+++ home/optional/sops.nix
@@ -0,0 +1,21 @@
+# Home level sops. See hosts/common/optiona/sops.nix for hosts level
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
+let
+  sopsFile = (builtins.toString inputs.nix-secrets) + "/sops.secret.yaml";
+  homeDirectory = config.home.homeDirectory;
+in
+{
+  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+  sops = {
+    age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
+    defaultSopsFile = sopsFile;
+    "keys/ssh/luna/ed25519" = {
+      path = "${homeDirectory}/.ssh/id_ed25519";
+    };
+  };
+}
