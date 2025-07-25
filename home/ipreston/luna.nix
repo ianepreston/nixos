757f87f1b1f3b4c731e36215e5a575bfa973d237
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-07-25 11:03:21 -0600

    get ssh keys managed in home-manager

--- home/ipreston/luna.nix
+++ home/ipreston/luna.nix
@@ -2,10 +2,10 @@
 {
   imports = [
     ../core/default.nix
-    # ../optional/bambustudio.nix
     ../optional/browser.nix
     ../optional/gnome
     ../optional/comms.nix
     ../optional/media.nix
+    ../optional/sops.nix
   ];
 }
