3f1518e94cabe9473114c8d8ef28fda7de32ce2c
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-08-02 18:35:29 -0600

    update dconf
    Add in more explicit stuff, why not?

--- home/optional/gnome/dconf.nix
+++ home/optional/gnome/dconf.nix
@@ -2,9 +2,166 @@
 {lib, ...}:
 with lib.hm.gvariant; {
   dconf.settings = {
+    "org/gnome/calculator" = {
+      base = 10;
+      button-mode = "basic";
+      source-currency = "";
+      source-units = "degree";
+      target-currency = "";
+      target-units = "radian";
+      window-maximized = false;
+      window-size = mkTuple [360 616];
+    };
+
+    "org/gnome/desktop/a11y/applications" = {
+      screen-reader-enabled = false;
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = ["System" "Utilities" "YaST" "Pardus"];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Pardus" = {
+      categories = ["X-Pardus-Apps"];
+      name = "X-Pardus-Apps.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/System" = {
+      apps = ["org.gnome.DiskUtility.desktop" "org.gnome.Logs.desktop" "org.gnome.SystemMonitor.desktop" "org.gnome.baobab.desktop"];
+      name = "X-GNOME-Shell-System.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = ["org.gnome.Connections.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.font-viewer.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop"];
+      name = "X-GNOME-Shell-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = ["X-SuSE-YaST"];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/break-reminders/eyesight" = {
+      play-sound = true;
+    };
+
+    "org/gnome/desktop/break-reminders/movement" = {
+      duration-seconds = mkUint32 300;
+      interval-seconds = mkUint32 1800;
+      play-sound = true;
+    };
+
+    "org/gnome/desktop/notifications" = {
+      application-children = ["firefox" "gnome-power-panel" "org-gnome-console" "discord" "spotify" "org-gnome-nautilus" "com-discordapp-discord" "org-gnome-software" "bambustudio" "signal"];
+    };
+
+    "org/gnome/desktop/notifications/application/bambustudio" = {
+      application-id = "BambuStudio.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/com-discordapp-discord" = {
+      application-id = "com.discordapp.Discord.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/discord" = {
+      application-id = "discord.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/firefox" = {
+      application-id = "firefox.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+      application-id = "gnome-power-panel.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-console" = {
+      application-id = "org.gnome.Console.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
+      application-id = "org.gnome.Nautilus.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-software" = {
+      application-id = "org.gnome.Software.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/signal" = {
+      application-id = "signal.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/spotify" = {
+      application-id = "spotify.desktop";
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/search-providers" = {
+      sort-order = ["org.gnome.Settings.desktop" "org.gnome.Contacts.desktop" "org.gnome.Nautilus.desktop"];
+    };
+
+    "org/gnome/desktop/sound" = {
+      theme-name = "ocean";
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "icon:minimize,maximize,close";
+    };
+
+    "org/gnome/evince/default" = {
+      continuous = true;
+      dual-page = false;
+      dual-page-odd-left = false;
+      enable-spellchecking = true;
+      fullscreen = false;
+      inverted-colors = false;
+      show-sidebar = true;
+      sidebar-page = "thumbnails";
+      sidebar-size = 586;
+      sizing-mode = "fit-page";
+      window-ratio = mkTuple [1.5117919774239066 1.0084093500570126];
+    };
+
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
+    "org/gnome/file-roller/listing" = {
+      list-mode = "as-folder";
+      name-column-width = 66;
+      show-path = false;
+      sort-method = "name";
+      sort-type = "ascending";
+    };
+
+    "org/gnome/file-roller/ui" = {
+      sidebar-width = 200;
+      window-height = 480;
+      window-width = 600;
+    };
+
     "org/gnome/mutter/keybindings" = {
       toggle-tiled-left = ["<Super>Left"];
       toggle-tiled-right = ["<Super>Right"];
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+    };
+
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = false;
+      night-light-schedule-automatic = false;
+      night-light-schedule-from = 19.0;
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
@@ -19,14 +176,39 @@ with lib.hm.gvariant; {
       name = "Launch Terminal";
     };
 
+    "org/gnome/shell" = {
+      enabled-extensions = ["user-theme@gnome-shell-extensions.gcampax.github.com"];
+      favorite-apps = ["org.gnome.Nautilus.desktop" "firefox.desktop" "org.gnome.Console.desktop"];
+      last-selected-power-profile = "performance";
+      welcome-dialog-last-shown-version = "48.1";
+    };
+
     "org/gnome/shell/extensions/paperwm" = {
       restore-keybinds = ''
-        {"toggle-tiled-left":{"bind":"[\\"<Super>Left\\"]","schema_id":"org.gnome.mutter.keybindings"},"toggle-tiled-right":{"bind":"[\\"<Super>Right\\"]","schema_id":"org.gnome.mutter.keybindings"}}
+        {"toggle-tiled-left":{"bind":"[\\\\"<Super>Left\\\\"]","schema_id":"org.gnome.mutter.keybindings"},"toggle-tiled-right":{"bind":"[\\\\"<Super>Right\\\\"]","schema_id":"org.gnome.mutter.keybindings"}}\n
       '';
+    };
+
+    "org/gnome/shell/extensions/paperwm/workspaces/9aee5735-5d95-4ec1-ac30-5c53ae4aff49" = {
+      index = 0;
+    };
+
+    "org/gnome/shell/extensions/user-theme" = {
+      name = "Stylix";
     };
 
     "org/gnome/shell/keybindings" = {
       show-screenshot-ui = ["<Shift><Super>s"];
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = [];
+    };
+
+    "org/gnome/software" = {
+      check-timestamp = mkInt64 1754161386;
+      first-run = false;
+      flatpak-purge-timestamp = mkInt64 1754178371;
     };
   };
 }
