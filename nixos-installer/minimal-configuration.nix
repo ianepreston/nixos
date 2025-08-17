51e0544ea1fff068365945b9fc92b704062704e5
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-08-17 17:00:13 -0600

    setup luna for full install

--- nixos-installer/minimal-configuration.nix
+++ nixos-installer/minimal-configuration.nix
@@ -18,20 +18,20 @@
   ];
 
   # Put this back when you're done with chromebook, it needs grub
-  # fileSystems."/boot".options = [ "umask=0077" ]; # Removes permissions and security warnings.
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.systemd-boot = {
-  #   enable = true;
-  #   # we use Git for version control, so we don't need to keep too many generations.
-  #   configurationLimit = lib.mkDefault 3;
-  #   # pick the highest resolution for systemd-boot's console.
-  #   consoleMode = lib.mkDefault "max";
-  # };
-  # boot.initrd = {
-  #   systemd.enable = true;
-  #   systemd.emergencyAccess = true; # Don't need to enter password in emergency mode
-  #   luks.forceLuksSupportInInitrd = true;
-  # };
+  fileSystems."/boot".options = [ "umask=0077" ]; # Removes permissions and security warnings.
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    # we use Git for version control, so we don't need to keep too many generations.
+    configurationLimit = lib.mkDefault 3;
+    # pick the highest resolution for systemd-boot's console.
+    consoleMode = lib.mkDefault "max";
+  };
+  boot.initrd = {
+    systemd.enable = true;
+    systemd.emergencyAccess = true; # Don't need to enter password in emergency mode
+    luks.forceLuksSupportInInitrd = true;
+  };
 
   boot.kernelParams = [
     "systemd.setenv=SYSTEMD_SULOGIN_FORCE=1"
