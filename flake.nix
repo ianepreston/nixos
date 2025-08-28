df083865a0eed44a3ab0fbaa98697a02cd0d29a6
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2025-08-27 18:25:27 -0600

    add chromebook tablet

--- flake.nix
+++ flake.nix
@@ -97,6 +97,16 @@
           ./home/work/workvm.nix
         ];
       };
+      homeConfigurations."penguin" = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = {
+          inherit customLib inputs;
+          hostSpec = hostSpecs.penguin;
+        };
+        modules = [
+          ./home/ipreston/penguin.nix
+        ];
+      };
     };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
