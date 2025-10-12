89353bf8131725ffe0027a67ef7d5c5b11761480
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-10-12 14:41:43 -0600

    add calibre to desktop
    Would really like to not need this eventually

--- home/ipreston/terra.nix
+++ home/ipreston/terra.nix
@@ -4,6 +4,7 @@
     ../core/default.nix
     ../optional/browser.nix
     ../optional/gnome
+    ../optional/calibre.nix
     ../optional/comms.nix
     ../optional/media.nix
     ../optional/sops.nix
