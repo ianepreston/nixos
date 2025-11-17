4dd6354551d5dc5448e5da737390d3c6d8ddad63
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-11-17 08:17:26 -0700

    allow clipboard read
    Otherwise I get prompted every time my clipboard has content and I try
    and yank

--- home/optional/gnome/ghostty.nix
+++ home/optional/gnome/ghostty.nix
@@ -5,6 +5,8 @@
     settings = {
       theme = "catppuccin-latte";
       font-family = "monospace";
+      clipboard-read = "allow";
+      clipboard-write = "allow";
       font-size = "11";
       # background-opacity = "0.85";
     };
