4db0c80eb6a3ca26041e92040fffd7a29e5da708
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-11-28 16:11:39 -0700

    add comments
    https://lazy.folke.io/configuration

--- home/core/neovim/lua/plugin-loader.lua
+++ home/core/neovim/lua/plugin-loader.lua
@@ -14,24 +14,36 @@ vim.opt.rtp:prepend(lazypath)
 
 local opts = {
   install = {
+    -- install missing plugins on startup. This doesn't increase startup time.
     missing = true,
+    -- try to load one of these colorschemes when starting an installation during startup
     colorscheme = { require("colorschemes.gruvbox").name },
   },
-  -- Directory for your local plugins. Uncomment if you'd like to use this feature -- useful when developing plugins
-  -- dev = { path = "~/nvim/projects" },
   defaults = {
+    -- Set this to `true` to have all your plugins lazy-loaded by default.
+    -- Only do this if you know what you are doing, as it can lead to unexpected behavior.
     lazy = true,
   },
-  ui = { wrap = "true" },
-  change_detection = { enabled = true },
-   -- automatically check for plugin updates
-  checker = { enabled = false },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = true, -- get a notification when changes are found
+  },
+  -- automatically check for plugin updates
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+    check_pinned = false, -- check for pinned packages that can't be updated
+  },
   debug = false,
   performance = {
     cache = {
       enabled = true,
     },
-    reset_packpath = true,
+    reset_packpath = true, -- reset the package path to improve startup time
     rtp = {
       disabled_plugins = {
         -- -- Disabled in many distros to cut few msec of startup time, but it allows remote development:
