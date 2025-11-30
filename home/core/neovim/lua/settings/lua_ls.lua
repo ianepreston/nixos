8fd6e83dccb482d7f8afd63b4a0040fa2a143410
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-11-30 16:22:29 -0700

    fix lua loading error

--- home/core/neovim/lua/settings/lua_ls.lua
+++ home/core/neovim/lua/settings/lua_ls.lua
@@ -16,15 +16,13 @@ local opts = {
         arrayIndex = "Disable",
       },
       workspace = {
+
         library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          -- Path of this config's lua directory
-          [vim.fn.stdpath "config" .. "/lua"] = true,
-          require("neodev.config").types(),
-          -- Fixing annoying neodev prompt about luv when editing standalone lua files
-          -- That's an issue in LunarVim: https://github.com/LunarVim/LunarVim/issues/4049
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.stdpath "config" .. "/lua",
           "${3rd}/luv/library",
         },
+
         -- Slightly less correct way to fix the issue mentioned above. Uncomment if can't configure above
         -- checkThirdParty = false,
       },
