6a1de2a6545f1710dbdf5edd1a8e3b4244b22d7f
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-12-08 18:47:38 -0700

    swap dressing for snacks
    dressing is archived, snacks is the hotness

---
+++ home/core/neovim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+local M = {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    input = {
+      enabled = true,
+    },
+    picker = {
+      enabled = true,
+      ui_select = true,
+    },
+    bufdelete = { enabled = true },
+  },
+}
+
+function M.config(_, opts)
+  require("snacks").setup(opts)
+end
