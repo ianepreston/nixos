179d1e32b9aeb71263a48dd787e8de4f4686931e
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-12-14 16:30:09 -0700

    handle nix envs for python

--- home/core/neovim/lsp/basedpyright.lua
+++ home/core/neovim/lsp/basedpyright.lua
@@ -1,5 +1,15 @@
 local opts = {
   settings = {
+    python = {
+      pythonPath = vim.fn.exepath "python", -- resolves from PATH at startup
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "openFilesOnly",
+        -- You can soften this while testing:
+        -- diagnosticSeverityOverrides = { reportMissingModuleSource = "information" },
+      },
+    },
     basedpyright = {
       analysis = {
         typeCheckingMode = "recommended",
