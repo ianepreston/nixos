fe958f2e83f6d3a6ecdf09b374ba6e8bf25a23e2
ianepreston <17241371+ianepreston@users.noreply.github.com>
2025-12-21 09:35:47 -0700

    add moonlight
    helpful for testing sunshine

--- home/ipreston/luna.nix
+++ home/ipreston/luna.nix
@@ -4,6 +4,7 @@
     ../core/default.nix
     ../optional/browser.nix
     ../optional/gnome
+    ../optional/moonlight.nix
     ../optional/comms.nix
     ../optional/media.nix
     ../optional/sops.nix
