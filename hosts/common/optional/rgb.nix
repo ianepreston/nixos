fd987df48203293b2668cfc55436c37e8a31eb8a
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2026-01-25 12:08:21 -0700

    couple extra configs

--- hosts/common/optional/rgb.nix
+++ hosts/common/optional/rgb.nix
@@ -6,6 +6,11 @@
   services.hardware.openrgb = {
     enable = true;
     package = pkgs.openrgb-with-all-plugins;
+    motherboard = "amd";
+    server = {
+      port = 6742;
+      autoStart = true;
+    };
   };
   hardware.i2c.enable = true;
   boot.kernelModules = [
