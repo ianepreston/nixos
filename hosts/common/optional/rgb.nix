005e7cf2efd813e7c10e1785735884f7d27ee2b9
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2026-01-25 13:51:24 -0700

    remove hallucinated bs

--- hosts/common/optional/rgb.nix
+++ hosts/common/optional/rgb.nix
@@ -7,10 +7,6 @@
     enable = true;
     package = pkgs.openrgb-with-all-plugins;
     motherboard = "amd";
-    server = {
-      port = 6742;
-      autoStart = true;
-    };
   };
   hardware.i2c.enable = true;
   boot.kernelModules = [
