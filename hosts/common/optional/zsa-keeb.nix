5b77c21c642bac1a66b51769acfb786bc2b2c45c
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2026-03-30 19:23:53 -0600

    maybe fix xremap

--- hosts/common/optional/zsa-keeb.nix
+++ hosts/common/optional/zsa-keeb.nix
@@ -2,4 +2,22 @@
 {
   hardware.keyboard.zsa.enable = true;
   environment.systemPackages = [ pkgs.keymapp ];
+
+  # Prevent systemd-logind from grabbing Voyager as a "power-switch" device.
+  # The Voyager exposes Consumer Control and System Control HID interfaces that
+  # trigger logind to "watch system buttons" with an exclusive grab. This blocks
+  # xremap from grabbing the device at boot (race condition). By clearing
+  # ID_INPUT_KEY before 70-power-switch.rules runs, logind ignores the device.
+  # Rule must be numbered 61-69 to run after 60-input-id but before 70-power-switch.
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "65-zsa-voyager-no-powerswitch";
+      destination = "/etc/udev/rules.d/65-zsa-voyager-no-powerswitch.rules";
+      text = ''
+        # ZSA Voyager: prevent systemd-logind power-switch grab (fixes xremap race)
+        # Clears ID_INPUT_KEY so 70-power-switch.rules won't tag as power-switch
+        SUBSYSTEM=="input", KERNEL=="event*", ATTRS{idVendor}=="3297", ATTRS{idProduct}=="1977", ENV{ID_INPUT_KEY}="0"
+      '';
+    })
+  ];
 }
