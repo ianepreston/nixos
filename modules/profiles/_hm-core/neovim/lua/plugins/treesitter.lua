5174310c4f7c61ede72a626937c0042cba4e50dd
ianepreston <17241371+ianepreston@users.noreply.github.com>
2026-04-05 12:58:58 -0600

    fix treesitter
    API changes on latest version

--- modules/profiles/_hm-core/neovim/lua/plugins/treesitter.lua
+++ modules/profiles/_hm-core/neovim/lua/plugins/treesitter.lua
@@ -7,146 +7,94 @@ local M = {
   enabled = true,
   build = ":TSUpdate",
   lazy = false,
-  -- event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     { "JoosepAlviste/nvim-ts-context-commentstring" },
     { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
     { "nvim-treesitter/nvim-treesitter-context", branch = "master" },
     { "nvim-tree/nvim-web-devicons" },
   },
-  opts = {
-    -- for ensure_installed, see config() below
-
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-
-    -- Automatically install missing parsers when entering buffer
-    -- set to false if you don't have tree-sitter CLI installed
-    auto_install = true,
-
-    highlight = {
-      enable = true,
-      disable = function(_, bufnr)
-        return vim.api.nvim_buf_line_count(bufnr) > 10000
-      end,
-    },
-    -- autotag = {
-    --   enable = true,
-    -- },
-    autopairs = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-      -- disable = { "python", "css" }
-    },
-
-    -- Treesitter module deprecated in favor of require("ts_context_commentstring").setup { ... }
-    -- context_commentstring = {
-    --   enable = true,
-    --   enable_autocmd = false,
-    -- },
-
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["ak"] = { query = "@block.outer", desc = "around block" },
-          ["ik"] = { query = "@block.inner", desc = "inside block" },
-          ["ac"] = { query = "@class.outer", desc = "around class" },
-          ["ic"] = { query = "@class.inner", desc = "inside class" },
-          ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-          ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-          ["af"] = { query = "@function.outer", desc = "around function " },
-          ["if"] = { query = "@function.inner", desc = "inside function " },
-          ["al"] = { query = "@loop.outer", desc = "around loop" },
-          ["il"] = { query = "@loop.inner", desc = "inside loop" },
-          ["aa"] = { query = "@parameter.outer", desc = "around argument" },
-          ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]k"] = { query = "@block.outer", desc = "Next block start" },
-          ["]f"] = { query = "@function.outer", desc = "Next function start" },
-          ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
-        },
-        goto_next_end = {
-          ["]K"] = { query = "@block.outer", desc = "Next block end" },
-          ["]F"] = { query = "@function.outer", desc = "Next function end" },
-          ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
-        },
-        goto_previous_start = {
-          ["[k"] = { query = "@block.outer", desc = "Previous block start" },
-          ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-          ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
-        },
-        goto_previous_end = {
-          ["[K"] = { query = "@block.outer", desc = "Previous block end" },
-          ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-          ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          [">K"] = { query = "@block.outer", desc = "Swap next block" },
-          [">F"] = { query = "@function.outer", desc = "Swap next function" },
-          [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
-        },
-        swap_previous = {
-          ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
-          ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-          ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
-        },
-      },
-    },
-
-    playground = {
-      enable = false,
-      disable = {},
-      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-      persist_queries = false, -- Whether the query persists across vim sessions
-      keybindings = {
-        toggle_query_editor = "o",
-        toggle_hl_groups = "i",
-        toggle_injected_languages = "t",
-        toggle_anonymous_nodes = "a",
-        toggle_language_display = "I",
-        focus_language = "f",
-        unfocus_language = "F",
-        update = "R",
-        goto_node = "<cr>",
-        show_help = "?",
-      },
-    },
-    rainbow = {
-      enable = false,
-      extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-      max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-    },
-  },
 }
 
-function M.config(_, opts)
-  opts.ensure_installed = require("settings.toolset").ts_languages
+function M.config()
+  local ensure_installed = require("settings.toolset").ts_languages
+
   require("ts_context_commentstring").setup {
     enable_autocmd = false,
   }
-  require("nvim-treesitter.config").setup(opts)
+
+  require("nvim-treesitter").setup({
+    ensure_installed = ensure_installed,
+    auto_install = true,
+    sync_install = false,
+    ignore_install = {},
+  })
+
+  -- Incremental selection
+  vim.keymap.set("n", "<C-space>", function()
+    require("nvim-treesitter.incremental_selection").init_selection()
+  end, { desc = "Init treesitter selection" })
+  vim.keymap.set("x", "<C-space>", function()
+    require("nvim-treesitter.incremental_selection").node_incremental()
+  end, { desc = "Increment treesitter selection" })
+  vim.keymap.set("x", "<bs>", function()
+    require("nvim-treesitter.incremental_selection").node_decremental()
+  end, { desc = "Decrement treesitter selection" })
+
+  -- Textobjects
+  require("nvim-treesitter-textobjects").setup({
+    select = {
+      lookahead = true,
+      keymaps = {
+        ["ak"] = { query = "@block.outer", desc = "around block" },
+        ["ik"] = { query = "@block.inner", desc = "inside block" },
+        ["ac"] = { query = "@class.outer", desc = "around class" },
+        ["ic"] = { query = "@class.inner", desc = "inside class" },
+        ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
+        ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
+        ["af"] = { query = "@function.outer", desc = "around function " },
+        ["if"] = { query = "@function.inner", desc = "inside function " },
+        ["al"] = { query = "@loop.outer", desc = "around loop" },
+        ["il"] = { query = "@loop.inner", desc = "inside loop" },
+        ["aa"] = { query = "@parameter.outer", desc = "around argument" },
+        ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
+      },
+    },
+    move = {
+      set_jumps = true,
+      goto_next_start = {
+        ["]k"] = { query = "@block.outer", desc = "Next block start" },
+        ["]f"] = { query = "@function.outer", desc = "Next function start" },
+        ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
+      },
+      goto_next_end = {
+        ["]K"] = { query = "@block.outer", desc = "Next block end" },
+        ["]F"] = { query = "@function.outer", desc = "Next function end" },
+        ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
+      },
+      goto_previous_start = {
+        ["[k"] = { query = "@block.outer", desc = "Previous block start" },
+        ["[f"] = { query = "@function.outer", desc = "Previous function start" },
+        ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
+      },
+      goto_previous_end = {
+        ["[K"] = { query = "@block.outer", desc = "Previous block end" },
+        ["[F"] = { query = "@function.outer", desc = "Previous function end" },
+        ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
+      },
+    },
+    swap = {
+      swap_next = {
+        [">K"] = { query = "@block.outer", desc = "Swap next block" },
+        [">F"] = { query = "@function.outer", desc = "Swap next function" },
+        [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
+      },
+      swap_previous = {
+        ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
+        ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+        ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
+      },
+    },
+  })
 end
 
 return M
