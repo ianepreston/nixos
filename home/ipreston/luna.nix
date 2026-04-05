9b6b0a1433f6955bd8261a6ed80fb0ac894b5b9a
ianepreston <17241371+ianepreston@users.noreply.github.com>
2026-04-05 06:35:10 -0600

    add claude to laptop

--- home/ipreston/luna.nix
+++ home/ipreston/luna.nix
@@ -4,6 +4,7 @@
     ../core/default.nix
     ../optional/browser.nix
     ../optional/gnome
+    ../optional/vibes.nix
     ../optional/moonlight.nix
     ../optional/comms.nix
     ../optional/media.nix
