fe2b7f4022dff5b624d0fc4f0ca08c3351b9d68d
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2026-06-07 13:22:22 -0600

    patch caddy hash

--- modules/system/caddy.nix
+++ modules/system/caddy.nix
@@ -74,7 +74,7 @@ _: {
               # renovate: datasource=github-tags depName=caddy-dns/cloudflare
               "github.com/caddy-dns/cloudflare@v0.2.4"
             ];
-            hash = "sha256-bzMqxWTqrJ1skZmRTXyEMCKStXpljbqe5r0Ve2cnBfM=";
+            hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
           };
           globalConfig = ''
             acme_dns cloudflare {env.CLOUDFLARE_API_TOKEN}
