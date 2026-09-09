2deb414eb5e6b0968072bccac9b0829a403f75db
ianepreston <17241371+ianepreston@users.noreply.github.com>
2026-09-09 05:41:11 -0600

    neovim: use non-deprecated vim.lsp.log.set_level
    vim.lsp.set_log_level is deprecated in Neovim 0.11+ (0.12.4 in this
    config) and emits a deprecation warning on every startup. Switch to the
    non-deprecated vim.lsp.log.set_level, which is a direct drop-in.
    
    Co-authored-by: Isaac <no-reply@databricks.com>

--- modules/programs/neovim/lua/options.lua
+++ modules/programs/neovim/lua/options.lua
@@ -76,7 +76,7 @@ opt.iskeyword:append "-" -- treats words with `-` as single words
 opt.formatoptions:remove { "c", "r", "o" } -- This is a sequence of letters which describes how automatic formatting is to be done
 -- opt.colorcolumn = { 80, 120 } -- set line width indication at columns 80 and 120
 opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-vim.lsp.set_log_level "off" -- LSP log gets huge and I don't care about it
+vim.lsp.log.set_level "off" -- LSP log gets huge and I don't care about it
 
 --- Get quarto to work as markdown
 vim.filetype.add {
